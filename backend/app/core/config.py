a41d33d89df202cdbb0e5e9db15d0146f53d52aa
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-01-16 22:39:05 +0300

    Refactor Alembic migration setup and update requirements for password hashing

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings  
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:123@localhost:5432/meetme" 
-    SECRET_KEY: str = "mysecretkey"  
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str 
+    SECRET_KEY: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES: int 
 
 
     class Config:
